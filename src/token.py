e47f3e1e990d2d905247139da58702cfe09abad6
quasi-derivative <43278934+quasi-derivative@users.noreply.github.com>
2022-08-09 16:07:24 +0200

    Add types

---
+++ src/token.py
@@ -0,0 +1,15 @@
+import token_type
+
+class Token:
+    def __init__(self, 
+            type:token_type.TokenType,
+            lexeme:str,
+            literal,
+            line:int):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __str__(self):
+        return f'{self.type} {self.lexeme} {self.literal} {self.line}'
---
+++ src/token_type.py
@@ -0,0 +1,22 @@
+from enum import Enum, auto
+
+class Ttype(Enum):
+    """
+    Enum containing all the types for 
+    """
+    # Data Types
+    INT = 'I'
+    INTS = 'Is'
+    REAL = 'R'
+    REALS = 'Rs'
+    STRING = 'S'
+    STRINGS = 'Ss'
+    BOOl = 'B'
+    BOOLS = 'Bs'
+
+    # Code Structuring
+    LEFTPAREN = '('
+    RIGHTPAREN = ')'
+    NEWLINE = '\n'
+    
+
