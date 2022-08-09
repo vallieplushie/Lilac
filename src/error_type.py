4ce575ce01054e767c932c2cd23b1b3815df5d8c
quasi-derivative <43278934+quasi-derivative@users.noreply.github.com>
2022-08-09 15:54:37 +0200

    Added error typing

---
+++ src/error_type.py
@@ -0,0 +1,12 @@
+from enum import Enum, auto
+
+class Error(Enum):
+    """
+    Contains the different types of error, as an enum
+    """
+    SyntaxError = 'SyntaxError'
+    MismatchedParenthesesError = 'MismatchedParenthesesError'
+    ArgumentTypeError = 'ArgumentTypeError'
+    NameUndefinedError = 'NameUndefinedError'
+    Blank = ''
+
--- src/lilac.py
+++ src/lilac.py
@@ -1,2 +1,56 @@
 import sys
 from typing import *
+
+from error_type import Error
+
+class Lilac:
+    """
+    Main Lilac class, contains the driver code
+    """
+    hadError:bool = false
+    interactive:bool = true
+
+    @staticmethod
+    def run_Prompt():
+        """
+        Runs the interactive prompt
+        """
+        print('====== Lilac Interactive Prompt ======')
+        print('Enter the command "quit" to exit.')
+        Lilac.interactive_mode()
+    
+    @staticmethod
+    def interactive_mode():
+        """
+        Interactive prompt
+        """
+        while True:
+            print('>', end=' ')
+            line = input()
+            if line == 'quit':
+                sys.exit()
+            Lilac.run(line)
+            Lilac.hadError = false
+
+    @staticmethod
+    def run(source:str):
+        """
+        Runs the source, this will build up all the different parts
+        """
+
+    @staticmethod
+    def throw(line:int, type:Error=Error.Blank, message:str):
+        Lilac.report(line, type, message)
+
+    @staticmethod
+    def report(line:int, err_type:Error, message:str):
+        """
+        Prints and reports an error to the console
+        """
+        print(f'[Line {line}] {err_type.value}: {message}')
+        Lilac.hadError = True
+        if Lilac.interactive:
+            Lilac.interactive_mode()
+        else:
+            sys.exit(64) # need to use the correct exit code here, maybe add different codes in the Error enum
+    
